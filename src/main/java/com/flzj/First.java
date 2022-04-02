e4ff7c721bc9d943c29a8fab7bc8278b8fe8c2c1
flzj_kl <kl@flzj.com>
2022-04-02 18:23:25 +0800

    my third commit

--- src/main/java/com/flzj/First.java
+++ src/main/java/com/flzj/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("hhaha");
         System.out.println("www");
+        System.out.println("12121212");
     }
 }
